21a0aa0b8f7b10dc47ee149f68ea04c50ae3ae3f
SrBeholder <danielvcs@gmail.com>
2016-05-15 21:50:33 -0300

    Adição do script base de manipulacao e exemplo dos traces

---
+++ scriptBase.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import os
+import math
+import re
+import gzip
+import glob
+import os.path
+import shutil
+from os import walk
+
+class Principal():
+	
+	#Diretório com arquivos compactados
+	caminhoOrigem = "/home/daniel/Dropbox/Live_Characterization/SampleTraces/Origem"
+	#Diretório de trabalho, é apagado no fim do código
+	caminhoDestino = "/home/daniel/Dropbox/Live_Characterization/SampleTraces/Destino"	
+
+
+	#Localiza todos os arquivos com extensão gz no diretório origem descompacta em arquivo na pasta destino
+	for arquivoGZ in glob.glob(os.path.join(caminhoOrigem, '*.gz')):
+	    base = os.path.basename(arquivoGZ)
+	    dest_name = os.path.join(caminhoDestino, base[:-3])
+	    with gzip.open(arquivoGZ, 'rb') as infile:
+	        with open(dest_name, 'w') as outfile:
+	            for line in infile:
+					#1)Escreve linha a linha
+	                #outfile.write(line)
+
+					#2)Avalia linha a linha
+					#Escreve a linha como vetor
+					linha = line.split()
+					print(linha[20])
+					
+	
