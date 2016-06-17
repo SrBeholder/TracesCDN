b244650c3b7acea0763138e9077277cbb8d06c80
SrBeholder <danielvcs@gmail.com>
2016-06-17 00:21:42 -0300

    Implementação de varredura em multiplos diretorios

--- scriptBase.py
+++ scriptBase.py
@@ -7,15 +7,23 @@ import gzip
 import glob
 import os.path
 import shutil
+import fnmatch
+import zipfile
 from os import walk
+
+
 
 dicionarioMobile = []
 dicionarioDesktop = []
 
-totalRegistros = 0;
-totalMobile = 0;
-totalDesktop = 0;
+global totalRegistros
+totalRegistros = 0
+global totalMobile
+totalMobile = 0
+global totalDesktop
+totalDesktop = 0
 
+#Defini dicionário de dispositivos móveis e não móveis
 def defineDicionarios():
     ArquivoMobile = open('Mobile.txt', 'r');
     for linha in ArquivoMobile:
@@ -25,63 +33,66 @@ def defineDicionarios():
     for linha in ArquivoDesktop:
         dicionarioDesktop.append(linha.strip())
 
-def EncontraTermo(linha):
+#Localiza se um termo dos dicionários para classificar registro
+#Registros não classificados são marcados como 'undefined'
+def EncontraTermo(linha,contadores):
     achou = False
     for termo in dicionarioMobile:
         if (linha.find(termo) != -1):
-            global totalMobile
-            totalMobile = totalMobile + 1
+            contadores[0] = contadores[0] + 1
             achou = True
             break
     if(achou == False):
         for termo in dicionarioDesktop:
             if (linha.find(termo) != -1):
-                global totalDesktop
-                totalDesktop = totalDesktop + 1
+                contadores[1] = contadores[1] + 1
                 achou = True
                 break
-    return achou
+    contadores[3] = achou
+    return contadores
+
+#Localiza todos os arquivos com extensão gz no diretório origem descompacta em arquivo na pasta destino
+def VarreDiretorio(caminho,contadores):
+    for conteudo in glob.glob(os.path.join(caminho, '*')):
+        print(conteudo)
+        if os.path.isdir(conteudo):
+            print("diretorio")
+            contadores = VarreDiretorio(conteudo,contadores)
+        if fnmatch.fnmatch(conteudo, '*.gz'):
+            print("arquivo")
+            base = os.path.basename(conteudo)
+            with gzip.open(conteudo, 'rb') as infile:
+                with open('Undefined.txt', 'a') as outfile:
+                    #Para cada linha do arquivo de entrada
+                    for linha in infile:
+                        contadores[2] = contadores[2] + 1
+                        #Transforma linha em vetor
+                        vetorLinha = linha.split()
+                        #Exclui todos as colunas antes da 20ª
+                        linhaLimpa = ' '.join(vetorLinha[20::])
+                        #Busca termos dos dicionários na linha, caso não encontre, escrever no dicionário "Undefined"
+                        contadores = EncontraTermo(linhaLimpa,contadores)
+                        if(contadores[3] == False):
+                            outfile.write(linhaLimpa+"\n")
+    return contadores
+
+
 
 class Principal():
+    #Contadores [0]totalMobile [1]totalDesktop [2]totalRegistros
+    contadores = [0,0,0,False]
+
     #Coloca em memória os dicionários
     defineDicionarios()
 
-    totalRegistros = 0;
-    totalMobile = 0;
-    totalDesktop = 0;
-
     #Diretório com arquivos compactados
-    #caminhoOrigem = "/home/daniel/Dropbox/Live_Characterization/SampleTraces/Origem"
-    #caminhoOrigem = "/home/posgrad/dvasconcelos/Downloads/SampleTraces"
+    #caminhoOrigem = "/media/servertrace/live"
     caminhoOrigem = "/home/daniel/workplace/TracesGlobo/Origem"
 
-    #Diretório de trabalho, é apagado no fim do código
-    #caminhoDestino = "/home/posgrad/dvasconcelos/Downloads/SampleTraces"    
-    caminhoDestino = "/home/daniel/workplace/TracesGlobo"
+    #Localiza traces    
+    contadores = VarreDiretorio(caminhoOrigem, contadores)
 
-    #Localiza todos os arquivos com extensão gz no diretório origem descompacta em arquivo na pasta destino
-    for arquivoGZ in glob.glob(os.path.join(caminhoOrigem, '*.gz')):
-        base = os.path.basename(arquivoGZ)
-        with gzip.open(arquivoGZ, 'rb') as infile:
-            with open('Undefined.txt', 'a') as outfile:
-                #Para cada linha do arquivo de entrada
-                for linha in infile:
-                    global totalRegistros
-                    totalRegistros = totalRegistros + 1
-                    #Transforma linha em vetor
-                    vetorLinha = linha.split()
-                    #Exclui todos as colunas antes da 20ª
-                    linhaLimpa = ' '.join(vetorLinha[20::])
-                    #Busca termos dos dicionários na linha, caso não encontre, escrever no dicionário "Undefined"
-                    if(EncontraTermo(linhaLimpa) == False):
-                        outfile.write(linhaLimpa+"\n")
-
-    global totalMobile
-    global totalDesktop
-    global totalRegistros
-    print(totalMobile)
-    print(totalDesktop)
-    print(totalRegistros)
+    print(contadores)
                  
     
 
